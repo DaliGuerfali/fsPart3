7faf94796a95194354cadefa1bbd843a2b464c7e
Dali Guerfali <daliguerfali@gmail.com>
2023-06-14 02:53:45 +0100

    added lint

--- index.js
+++ index.js
@@ -28,56 +28,56 @@ app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(res => {
     response.json(res);
   })
-  .catch(err => next(err));
+    .catch(err => next(err));
 });
 
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    Person.findById(id).then(res => {
-      if(res) {
-        response.json(res);
-      } else {
-        next();
-      }
+  const id = request.params.id;
+  Person.findById(id).then(res => {
+    if(res) {
+      response.json(res);
+    } else {
+      next();
+    }
 
-    })
+  })
     .catch(err => next(err));
 });
 
 
 app.get('/api/info', (request, response, next) => {
-    Person.find({}).then(res => {
-      response.send(`
+  Person.find({}).then(res => {
+    response.send(`
       <p>Phonebook has info for ${res.length} people</p>
       <p>${new Date()}</p>
       `);
-    })
+  })
     .catch(err => next(err));
 });
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
+  const id = request.params.id;
 
-    Person.findByIdAndRemove(id).then(res => {
-      response.status(204).end();
-    })
+  Person.findByIdAndRemove(id).then(() => {
+    response.status(204).end();
+  })
     .catch(err => next(err));
 });
 
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body;
+  const body = request.body;
 
-    const person = new Person({
-      name: body.name,
-      number: body.number
-    });
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
 
-    person.save().then(res => {
-      response.json(res);
-    })
+  person.save().then(res => {
+    response.json(res);
+  })
     .catch(err => next(err));
 });
 
@@ -86,17 +86,17 @@ app.put('/api/persons/:id',(request,response, next) => {
   const id = request.params.id;
 
   const { name, number } = request.body;
-  
-  Person.findByIdAndUpdate(id, {name, number}, { new: true, runValidators: true, context: 'query' })
-  .then(res => {
-    response.json(res);
-  })
-  .catch(err => next(err));
+
+  Person.findByIdAndUpdate(id, { name, number }, { new: true, runValidators: true, context: 'query' })
+    .then(res => {
+      response.json(res);
+    })
+    .catch(err => next(err));
 });
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
-}
+};
 
 app.use(unknownEndpoint);
 
@@ -110,7 +110,7 @@ const errorHandler = (error, request, response, next) => {
   }
 
   next(error);
-}
+};
 
 app.use(errorHandler);
 
